e55c7d2ccdeffc650540cecab1416d461ae786e6
erratbi <yerratbi@gmail.com>
2023-08-29 12:22:41 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+    'plugin:tailwindcss/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-var': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
+      config: './tailwind.config.ts',
+    },
+    next: {
+      rootDir: ['./'],
+    },
+  },
+};
+
+module.exports = config;
